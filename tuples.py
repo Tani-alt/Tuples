1747fb45827a6295fc419d3b2baef25a3555d3b5
Tani-alt <Tanirika.gupta@gmail.com>
2025-03-19 10:09:31 +0530

    x

---
+++ tuples.py
@@ -0,0 +1,11 @@
+tuplex=("tuple", False, 3.2,1)
+print(tuplex)
+tuplex=(4,6,2,8,3,1)
+print(tuplex)
+tuple1=(50,60,70,80)
+print(tuple1.count(50))
+tuplex=(2,4,5,8,6,9,1,3)
+_slice=tuplex[3:5]
+print(_slice)
+_slice=tuplex[:6]
+print(_slice)
